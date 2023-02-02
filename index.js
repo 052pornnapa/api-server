fe8c732787b9361b39fbfa3a903d3fa3de1f9e62
052pornnapa <pornnapaep01@gmail.com>
2023-02-02 20:09:35 +0700

    text

--- index.js
+++ index.js
@@ -1,13 +1,105 @@
 //import { express } from "express";
 const express = require('express')
+const mysql = require('mysql')
 
 const app = express();
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('hello word');
 
-});
 
-app.listen(3000,() =>
-console.log('server started'),
+const connecttion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'api',
+})
+
+connecttion.connect((err) => {
+    if (err) {
+        console.log('error connecting = ', ess)
+        return;
+    }
+
+    console.log('Success connecting');
+})
+
+
+
+app.post('/create', (req, res) => {
+    const { email, name, password } = req.body;
+    try {
+        connecttion.query(
+            "INSERT INTO user(email,name,password) VALUES(?,?,?)",
+            [email, name, password],
+            (err, result, fillters) => {
+                if (err) {
+                    console.log('error insterting', err);
+                    return res.status(400).send();
+
+                }
+                return res.status(201).json({ message: 'New user Successfully created' });
+            }
+        )
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send();
+    }
+})
+
+app.get('/read', async (req, res) => {
+    try {
+        connecttion.query("SELECT * FROM user", (err, result, fillters) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).send();
+            }
+            res.status(200).json(result)
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send();
+    }
+
+})
+
+app.patch('/update/:email', async (req, res) => {
+    const email = req.params.email;
+    const newPassword = req.body.newPassword;
+    try {
+        connecttion.query("UPDATE user SET password = ? WHERE email = ?", [newPassword, email], (err, result, fields) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).send();
+            }
+            res.status(200).json({ message: "User password updated successfully" })
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send;
+    }
+})
+
+app.delete("/delete/:email", async (req, res) => {
+    const email = req.params.email;
+    try {
+        connecttion.query("DELETE FROM user WHERE id = ?", [email], (err, result, fields) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).send();
+            }
+            if (result.affectedRows === 0) {
+                return res.status(404).json({ message: "No user with email" });
+            }
+            return res.status(200).json({ message: "User deleted successfully" });
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send;
+    }
+})
+
+
+app.listen(3001, () =>
+    console.log('server started'),
 );
+
